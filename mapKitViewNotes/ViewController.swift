187c8b4488641052d4535aa5dbe0e9b187b6bef3
Daniel Vega <daniel.vega23@d155.org>
2022-01-28 09:40:43 -0600

    map view finished first 3 videos, just have to finish last video

--- mapKitViewNotes/ViewController.swift
+++ mapKitViewNotes/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var mapViewOutlet: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        mapViewOutlet.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = CLLocationCoordinate2D(latitude: 42.2371, longitude: -88.3226)
+        let center2 = locationManager.location!.coordinate
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1600, longitudinalMeters: 1600)
+        let region2 = MKCoordinateRegion(center: center2, span: span)
+        mapViewOutlet.setRegion(region2, animated: true)
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+        
     }
 
 
